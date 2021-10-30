b391b3dfaefbdfc0dd4758c31fe584d1bc74f492
staileb <saidtaileb@gmail.com>
2021-10-31 00:03:35 +0200

    modifié :         Introduction.cpp

--- Introduction.cpp
+++ Introduction.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
+#include <numeric>
 using namespace std; // make name from std visible without std  
 
 double square(double x){ // function that returns a quare value of a double 
@@ -27,14 +28,39 @@ void some_function(){
   for (int i = 0; i < 3; ++i)
     {
       cout << "vector line is " << i << endl; 
-    }
+    }  
   return; 
+}
+
+// The scope of definitions and there liftime  
+
+vector<int> vec; // vector vec is global
+
+struct Record {
+  string name; 
+};
+
+void fct(int arg){ // fct is global while arg is local 
+
+  string motto {"who dares wins"}; // motto is local
+  auto p = new Record {"Hume"};    // p points to an unnamed Record (created by new)
+  cout << "this is the pointer " << p << endl; 
+}
+
+void constants() {
+  constexpr int dmv = 17;       // constant computed when the compiler is running
+  int var = 17;                 // integer is not constant
+  const double sqr = sqrt(var); // constant computed at the run time.
+  cout << "type of constants " << dmv << sqr << endl;
+  vector<double> v {1.2,2.4,4.5}; // v is not constant
+  //   const double s1 = sum(v); // the sum is evaluated at running time 
 }
 
 int main(){
   
   print_square(1.234);
   some_function() ; 
-
+  fct(2);
+  constants();
   return 0;
 }
